a605942fa782bdd8de5020ac754afce1f6db7bc7
Rakesh <rakeshsagartfi21@gmail.com>
2025-11-21 10:17:57 +0000

    add dismissible banner to Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,55 @@
 import {Component} from 'react'
+import {MdClose} from 'react-icons/md'
 import Header from '../Header'
 import SideBar from '../SideBar'
 
-import {HomeContainer, HomeContent} from './styledComponents'
+import {
+  HomeContainer,
+  HomeContent,
+  HomeBannerVideos,
+  BannerContainer,
+  BannerContent,
+  BannerNxtWatchLogo,
+  BannerText,
+  GetItButton,
+  CloseButton,
+} from './styledComponents'
 
 class Home extends Component {
+  state = {
+    bannerDisplay: true,
+  }
+
+  handleClose = () => {
+    this.setState({
+      bannerDisplay: false,
+    })
+  }
+
   render() {
+    const {bannerDisplay} = this.state
     return (
       <HomeContainer>
         <Header />
         <HomeContent>
           <SideBar />
+          <HomeBannerVideos>
+            <BannerContainer bannerDisplay={bannerDisplay}>
+              <BannerContent>
+                <BannerNxtWatchLogo
+                  src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+                  alt="nxt watch logo"
+                />
+                <BannerText>
+                  Buy Nxt Watch Premium prepaid plans with UPI
+                </BannerText>
+                <GetItButton type="button">GET IT NOW</GetItButton>
+              </BannerContent>
+              <CloseButton type="button" onClick={this.handleClose}>
+                <MdClose style={{width: '100%', height: '100%'}} />
+              </CloseButton>
+            </BannerContainer>
+          </HomeBannerVideos>
         </HomeContent>
       </HomeContainer>
     )
--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -17,3 +17,91 @@ export const HomeContent = styled.div`
     flex-direction: row;
   }
 `
+
+export const HomeBannerVideos = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  flex-grow: 1;
+  overflow-y: auto;
+`
+
+export const BannerContainer = styled.div`
+  display: ${props => (props.bannerDisplay === true ? 'flex' : 'none')};
+  justify-content: space-between;
+  align-items: flex-start;
+  background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
+  padding: 30px;
+  @media screen and (min-width: 768px) {
+    background-size: cover;
+    padding: 30px 26px;
+    background-position: center;
+  }
+`
+
+export const BannerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+export const BannerNxtWatchLogo = styled.img`
+  width: 146px;
+  @media screen and (min-width: 768px) {
+    width: 130px;
+  }
+`
+
+export const BannerText = styled.p`
+  font-family: 'Roboto';
+  font-size: 19px;
+  line-height: 1.6;
+  color: #1e293b;
+  width: 80%;
+  margin-bottom: 30px;
+  font-weight: 400;
+  @media screen and (min-width: 768px) {
+    font-size: 17px;
+    width: 92%;
+    font-weight: 500;
+  }
+`
+export const GetItButton = styled.button`
+  display: flex;
+  justify-content: center:
+  align-items: center;
+  font-family: "Roboto";
+  padding: 12px 18px;
+  font-size: 16px;
+  font-weight: bold;
+  color: #1e293b;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+  border: 1px solid #1e293b;
+  @media screen and (min-width: 768px){
+    padding: 10px 16px;
+    background-color: #ffffff;
+    font-size: 15px;
+  }
+  @media screen and (min-width: 1200px){
+    padding: 12px 16px;
+  }
+`
+
+export const CloseButton = styled.button`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background-color: transparent;
+  @media screen and (min-width: 768px) {
+    width: 34px;
+    height: 34px;
+  }
+`
