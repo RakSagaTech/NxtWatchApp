6acdce29b5a900bf4091e3293af6a87dfa615c8c
Rakesh <rakeshsagartfi21@gmail.com>
2025-11-21 10:08:17 +0000

    add MenuContext and SideBar component, update Home layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,17 @@
 import {Component} from 'react'
 import Header from '../Header'
+import SideBar from '../SideBar'
 
-import {HomeContainer} from './styledComponents'
+import {HomeContainer, HomeContent} from './styledComponents'
 
 class Home extends Component {
   render() {
     return (
       <HomeContainer>
         <Header />
+        <HomeContent>
+          <SideBar />
+        </HomeContent>
       </HomeContainer>
     )
   }
--- src/components/Home/styledComponents.js
+++ src/components/Home/styledComponents.js
@@ -3,7 +3,17 @@ import styled from 'styled-components'
 export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
-  background-color: "#f8fafc";
+  background-color: '#f8fafc';
   height: 100vh;
   width: 100vw;
+`
+
+export const HomeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `
